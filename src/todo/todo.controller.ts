47e42f441eb4b759c935fd92c6123149dd31c692
KimMinSeok <kmsaa@naver.com>
2023-11-26 22:56:16 +0900

    feat: add todo controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,7 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
 
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+  async createTodo(
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    return this.todoService.createTodo(title, content);
+  }
+
+  @Get()
+  async fetchTodos() {
+    return this.todoService.fetchTodos();
+  }
+
+  @Get(':id')
+  async fetchTodo(@Param('id') id: number) {
+    return this.todoService.fetchTodo(id);
+  }
+
+  @Patch(':id')
+  async updateTodo(
+    @Param('id') id: number,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    return this.todoService.updateTodo(id, title, content);
+  }
+
+  @Delete(':id')
+  async removeTodo(@Param('id') id: number) {
+    return this.todoService.removeTodo(id);
+  }
 }
