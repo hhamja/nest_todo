0f3ae6a9dfcd965bd8986338879275dc13b00a57
KimMinSeok <kmsaa@naver.com>
2023-11-26 22:34:34 +0900

    feat : todo CRUD 로직 추가

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -10,4 +10,45 @@ export class TodoService {
     @InjectRepository(Todo)
     private todoRepository: Repository<Todo>,
   ) {}
+
+  async createTodo(title: string, content: string) {
+    const todo: Todo = new Todo();
+    todo.title = title;
+    todo.content = content;
+
+    return this.todoRepository.save(todo);
+  }
+
+  // 모든 Todo 리스트 가져오기
+  async fetchTodos() {
+    return this.todoRepository.find();
+  }
+
+  // 특정 Todo 정보 가져오기
+  async fetchTodo(id: number) {
+    return this.todoRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  // 특정 Todo 수정하기
+  async updateTodo(id: number, title: string, content: string) {
+    const todo: Todo = await this.todoRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    todo.title = title;
+    todo.content = content;
+
+    return todo;
+  }
+
+  // 특정 Todo 삭제
+  async removeTodo(id: number) {
+    return await this.todoRepository.delete(id);
+  }
 }
