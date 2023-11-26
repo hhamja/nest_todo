9e56574bdd297c85e9527b41b0ed7029ac4f1569
KimMinSeok <kmsaa@naver.com>
2023-11-26 21:59:05 +0900

    feat : add todo repository in service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Todo } from './model/todo';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(Todo)
+    private todoRepository: Repository<Todo>,
+  ) {}
+}
